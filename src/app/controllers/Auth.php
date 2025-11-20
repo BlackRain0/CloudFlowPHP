63425eb5aea6ba5dc538008899ae340918db75af
BlackRain0 <ohnnyrain@gmail.com>
2025-11-20 14:36:29 +0700

    Added simple Router and Authorization for users

---
+++ src/app/controllers/Auth.php
@@ -0,0 +1,68 @@
+<?php
+namespace app\controllers;
+use app\util\Router;
+use app\util\Connect;
+
+
+class Auth{
+    public static function registration($formData, $formFile){
+        $email = $formData['email'];
+        $name = $formData['name'];
+        $pass = $formData['pass'];
+        $confirmPass = $formData['confirmPass'];
+        $photo = $formFile['photo'];
+
+        if($pass !== $confirmPass){
+            Router::errors('500');
+        }
+
+        $fileName = time(). '_' . $photo['name'];
+        $path = "uploads/photo/" . $fileName;
+
+        if(move_uploaded_file($photo["tmp_name"], $path)){
+            $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+            $queryReg = "INSERT INTO `users`(`id`, `name`, `mail`, `password`, `photo`)
+             VALUES (null,'$name','$email','$pass', '$photo')";
+             $reg = mysqli_query(Connect::connect(), $queryReg);
+
+             header("Location: ../");
+        }else{
+            Router::errors('500');
+        }
+
+    }
+
+    public static function auth($data){
+        $email = $data["email"];
+        $pass = $data["pass"];
+
+        $queryAllUsers = "SELECT * FROM `users`";
+        $allUsers = mysqli_query(Connect::connect(), $queryAllUsers);
+        $allUsers = mysqli_fetch_all($allUsers);
+
+        foreach($allUsers as $user){
+            if($user[2] == $email){
+                if(password_verify($pass, $user[3])){
+                    session_start();
+                    $_SESSION["email"] = $user[2];
+                    header("Location: ../");
+                    break;
+                }
+            }else{
+                echo "User not found";
+                break;
+            }
+        }
+    }
+
+    public static function logout(){
+
+        unset($_SESSION["email"]);
+        header('Location: ../');
+    }
+    
+
+}
+
+?>
--- src/app/utils/Router.php
+++ src/app/utils/Router.php
@@ -15,11 +15,68 @@ class Router{
     public static function postUrl($url, $controller, $method, $data=null, $file=null){
         self::$pagesList[] =[
             "url" => $url,
-            "controller" => $controller,
+            "class" => $controller,
             "method" => $method,
             "data" => $data,
             "file" => $file,
-        ]
+            "post" => true,
+        ];
+    }
+
+    public static function getContent(){
+        $get = $_GET['q'] ?? '';
+        foreach(self::$pageList as $val){
+            if($val['url'] === '/'.$get){
+                if($_SERVER["REQUEST_METHOD"] === 'POST'){
+                    $action = new $val['class'];
+                    $method = $val['method'];
+                    switch($method){
+                        case 'createGroup':
+                            $action -> $method($_POST);
+                            break;
+                        case "redactGroupName":
+                            $action -> $method($_POST);
+                            break;
+                        case "deleteGroup":
+                            $action -> $method($_POST);
+                            break;
+                        case "addUserToGroup":
+                            $action -> $method($_POST);
+                            break;
+                        case "deleteUserFromGroup":
+                            $action -> $method($_POST);
+                            break;
+                        case 'createTask':
+                            $action -> $method($_POST);
+                            break;
+                        case "redactTask":
+                            $action -> $method($_POST);
+                            break;
+                        case "deleteTask":
+                            $action -> $method($_POST);
+                            break;
+                        case "registrationUser":
+                            $action -> $method($_POST, $_FILES);
+                            break;
+                        case "authUser":
+                            $action -> $method($_POST);
+                            break;
+                        case "logout":
+                            $action -> $method();
+                            break;
+                    }
+                }else{
+                    require_once "views/pages/" . $val["namePage"] . '.php';
+                    die();
+                }
+            }
+        }
+        self::errors('404');
+    }
+
+    public static function errors($err){
+        require_once "views/errors/" . $err . '.php';
+        die();
     }
 
 
