30d8ecd741cb710e57408efb1313d3b69ae360dd
BlackRain <Ohnnyrain@gmail.com>
2025-11-27 11:45:51 +0700

    Added bootstrap

--- src/views/home.php
+++ src/views/home.php
@@ -4,8 +4,14 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home</title>
+	<link href="/bs/bootstrap.min.css" reel="stylesheet">
 </head>
 <body>
-	sos
+	<div class="header">
+		<div class="container">
+
+		</div>
+	</div>
+	<script src="/bs/bootstrap.bundle.min.js"></script>
 </body>
 </html>
